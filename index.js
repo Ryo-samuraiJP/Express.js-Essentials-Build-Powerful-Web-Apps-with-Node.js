8619cbb02ab1f9798a1b4f013e85af0190767fc6
Rio <onlytru6340@gmail.com>
2024-09-09 02:45:42 -0700

    Create a basic data endpoint

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-import express from "express"; 
+import express, { response } from "express"; 
 // import data from "./data/mock.json";
 import data from "./data/mock.json" assert { type: "json" };
-
 
 const app = express(); 
 
 const PORT = 3000;
+
+// GET
+app.get('/', (request, response) => {
+  response.json(data);
+});
+
+// POST
+app.post('/create', (request, response) => {
+  response.send("This is a POST request at /");
+});
+
+// PUT
+app.put('/edit', (request, response) => {
+  response.send("This is a PUT request at /");
+});
+
+// DELETE
+app.delete('/delete', (request, response) => {
+  response.send("This is a DELETE request at /");
+});
+
 
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}.`);
